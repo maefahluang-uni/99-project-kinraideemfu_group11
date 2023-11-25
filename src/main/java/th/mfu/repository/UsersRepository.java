573e07ec812e88ed2b6fbb76b6a64efb9472d1d0
Pakpoom-Rodjana <6531503062@lamduan.mfu.ac.th>
2023-11-26 06:16:40 +0700

    delete

--- src/main/java/th/mfu/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package th.mfu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import th.mfu.model.UsersModel;
-
-public interface UsersRepository extends JpaRepository <UsersModel, Long>{
-    UsersModel findByLoginAndPassword(String login, String password);
-}
